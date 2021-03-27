f7508818cc666405b030abad5c85c7ad4a85bbef
casperbangstad <49062874+casperbangstad@users.noreply.github.com>
2021-03-27 21:13:51 +0100

    test
    test

--- js/greeting.js
+++ js/greeting.js
@@ -4,6 +4,7 @@ const hour = today.getHours();
 
 // Here you can change your name
 const name = ' John Doe';
+const name = ' Casper';
 
 // Here you can change your greetings
 const gree1 = 'Go to Sleep!  ';
